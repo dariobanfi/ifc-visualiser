d1e7d8fb84c42c294a0d7b5550bb571376467873
Dario Banfi <dario.banfi@tum.de>
2014-12-23 16:54:34 +0100

    Bim server api connection

---
+++ IFCVisualiser/IFCVisualiser/Server/BIM/BIMServerApi.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using IFCVisualiser.Server.Model;
+using RestSharp;
+
+namespace IFCVisualiser.Server.BIM
+{
+    class BimServer
+    {
+
+        public static String BaseUrl = "http://data.ksd.ai.ar.tum.de:8080/";
+        public static String Username = "dario.banfi@tum.de";
+        public static String Password = "testpasssword";
+        
+        private String _token = null;
+        private RestClient _restClient;
+
+
+        public BimServer()
+        {
+            _restClient = new RestClient(BaseUrl);
+
+            authenticate();
+        }
+
+
+        private Boolean authenticate()
+        {
+            var request = new RestRequest(Method.POST);
+            request.Resource = "json/login";
+            request.RequestFormat = DataFormat.Json;
+
+            var authRequest = new AuthenticationRequest();
+            authRequest.request.parameters.username = Username;
+            authRequest.request.parameters.password = Password;
+
+            request.AddBody(authRequest);
+
+            var response = _restClient.Execute<AuthenticationResponse>(request);
+
+            if (response.Data.response.exception != null)
+            {
+                return false;
+            }
+
+            _token = response.Data.response.result;
+            return true;
+            
+        }
+
+        public void checkout()
+        {
+            if (_token == null)
+                authenticate();
+
+        }
+    }
+}
---
+++ IFCVisualiser/IFCVisualiser/Server/Model/AuthenticationRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApi
+{
+    public class Parameters
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    public class Request
+    {
+        public string @interface = "Bimsie1AuthInterface";
+        public string method = "login";
+        public Parameters parameters { get; set; }
+
+        public Request()
+        {
+            parameters = new Parameters();
+        }
+    }
+
+    public class AuthenticationRequest
+    {
+        public Request request { get; set; }
+
+        public AuthenticationRequest()
+        {
+            request = new Request();
+        }
+    }
+}
