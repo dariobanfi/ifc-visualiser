f1e515371a896f0ee000136e26d4275090b35939
Dario Banfi <dario.banfi@tum.de>
2015-01-06 17:26:15 +0100

    Small project fixing

--- IFCVisualiser/IFCVisualiser/Server/BIM/BIMServerApi.cs
+++ IFCVisualiser/IFCVisualiser/Server/BIM/BIMServerApi.cs
@@ -1,56 +1,202 @@
 ﻿using System;
-using IFCVisualiser.Server.Model;
-using RestSharp;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace IFCVisualiser.Server.BIM
+namespace TestApi
 {
     class BimServer
     {
 
         public static String BaseUrl = "http://data.ksd.ai.ar.tum.de:8080/";
         public static String Username = "dario.banfi@tum.de";
-        public static String Password = "testpasssword";
+        public static String Password = "testpassword";
+
+        public static String Ifc2XSersializer = "3276838";
         
-        private String _token = null;
-        private RestClient _restClient;
+        private String _token;
 
-
-        public BimServer()
+        public void Login()
         {
-            _restClient = new RestClient(BaseUrl);
 
-            authenticate();
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(BaseUrl + "json/login");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Proxy = null;
+            httpWebRequest.Method = "POST";
+
+            var rq = new BimRequest();
+            rq.request.method = "login";
+            rq.request.@interface = "Bimsie1AuthInterface";
+            rq.request.parameters.username = Username;
+            rq.request.parameters.password = Password;
+
+            var requestPayload = JsonConvert.SerializeObject(rq, Newtonsoft.Json.Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = requestPayload;
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                var httpResponse = (HttpWebResponse) httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var jsonResult = streamReader.ReadToEnd();
+                    var result = JsonConvert.DeserializeObject<BimResponse>(jsonResult);
+                    if (result.response.exception != null)
+                    {
+                        // TODO Ask for valid credentials
+                    }
+                    else
+                    {
+                        _token = result.response.result;
+                    }
+                }
+            }
         }
 
-
-        private Boolean authenticate()
+        private String LatestRoidRequest(int poid)
         {
-            var request = new RestRequest(Method.POST);
-            request.Resource = "json/login";
-            request.RequestFormat = DataFormat.Json;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(BaseUrl + "json/getProjectByPoid");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Proxy = null;
+            httpWebRequest.Method = "POST";
 
-            var authRequest = new AuthenticationRequest();
-            authRequest.request.parameters.username = Username;
-            authRequest.request.parameters.password = Password;
+            var rq = new BimRequest();
+            rq.token = _token;
+            rq.request.@interface = "Bimsie1ServiceInterface";
+            rq.request.method = "getProjectByPoid";
+            rq.request.parameters.poid = poid.ToString();
 
-            request.AddBody(authRequest);
+            var requestPayload = JsonConvert.SerializeObject(rq, Newtonsoft.Json.Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 
-            var response = _restClient.Execute<AuthenticationResponse>(request);
-
-            if (response.Data.response.exception != null)
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
             {
-                return false;
+                string json = requestPayload;
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var jsonResult = streamReader.ReadToEnd();
+                    dynamic stuff = JObject.Parse(jsonResult);
+                    try
+                    {
+                        return stuff.response.result.lastRevisionId;
+                    }
+                    catch (Exception)
+                    {
+                    }
+
+                }
             }
 
-            _token = response.Data.response.result;
-            return true;
-            
+            return null;
         }
 
-        public void checkout()
+        private String DownloadRequest(string roid, string serializerId)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(BaseUrl + "json/download");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Proxy = null;
+            httpWebRequest.Method = "POST";
+
+            var rq = new BimRequest();
+            rq.token = _token;
+            rq.request.method = "download";
+            rq.request.@interface = "Bimsie1ServiceInterface";
+            rq.request.parameters.roid = roid;
+            rq.request.parameters.serializerOid = serializerId;
+            rq.request.parameters.showOwn = "false";
+            rq.request.parameters.sync = "false";
+
+            var requestPayload = JsonConvert.SerializeObject(rq, Newtonsoft.Json.Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = requestPayload;
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var jsonResult = streamReader.ReadToEnd();
+                    var result = JsonConvert.DeserializeObject<BimResponse>(jsonResult);
+                    var opValue = result.response.result;
+
+                    return opValue;
+                }
+            }
+
+        }
+
+        private String getData(string code)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(BaseUrl + "json/getDownloadData");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Proxy = null;
+            httpWebRequest.Method = "POST";
+
+            var rq = new BimRequest();
+            rq.token = _token;
+            rq.request.method = "getDownloadData";
+            rq.request.@interface = "Bimsie1ServiceInterface";
+            rq.request.parameters.actionId = code;
+
+            var requestPayload = JsonConvert.SerializeObject(rq, Newtonsoft.Json.Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = requestPayload;
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var jsonResult = streamReader.ReadToEnd();
+                    dynamic resp = JObject.Parse(jsonResult);
+                    string jsonResponse = resp.response.result.file;
+                    
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter("downloadedIfc.ifc"))
+                    {
+                        var bytes = Convert.FromBase64String(jsonResponse);
+                        string ifcDecoded = Encoding.UTF8.GetString(bytes);
+                        file.Write(ifcDecoded);
+
+                    }
+                    return null;
+                }
+            }
+        }
+
+
+        public void Download(int id)
         {
             if (_token == null)
-                authenticate();
+                Login();
+
+            var roid = LatestRoidRequest(id);
+            var opcode = DownloadRequest(roid, Ifc2XSersializer);
+            var filePath = getData(opcode);
+
+
 
         }
     }
---
+++ IFCVisualiser/IFCVisualiser/Server/Model/BimResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApi
+{
+    public class BimResponse
+    {
+        public Response response { get; set; }
+    }
+
+
+    public class Response
+    {
+        public string result { get; set; }
+        public int lastRevisionId { get; set; }
+        public Exception exception { get; set; }
+        public string name { get; set; }
+
+    }
+
+
+    public class Exception : System.Exception
+    {
+        public string __type { get; set; }
+        public string message { get; set; }
+    }
+
+
+
+
+ 
+}
