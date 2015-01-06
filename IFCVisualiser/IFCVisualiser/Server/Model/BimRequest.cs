5cd006117badcac3c75b3171cdb135abad30fb56
Dario Banfi <dario.banfi@tum.de>
2015-01-06 17:19:58 +0100

    Completed bim downloading

---
+++ IFCVisualiser/IFCVisualiser/Server/Model/BimRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApi
+{
+
+    public class BimRequest
+    {
+
+        public String token { get; set; }
+        public Request request { get; set; }
+
+        public BimRequest()
+        {
+            request = new Request();
+        }
+    }
+
+    public class Request
+    {
+        public string @interface = "";
+        public string method { get; set; }
+        public Parameters parameters { get; set; }
+
+        public Request()
+        {
+            parameters = new Parameters();
+        }
+    }
+
+    public class Parameters
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string poid { get; set; }
+        public string roid { get; set; }
+        public string serializerOid{ get; set; }
+        public string showOwn{ get; set; }
+        public string sync{ get; set; }
+        public string actionId { get; set; }
+
+    }
+
+
+
+
+}
