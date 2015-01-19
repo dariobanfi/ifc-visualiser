5b84ccb997bbc9ca6d91f1b99151b6f96f51b78d
Jana Pejic <jana.pejic.9@gmail.com>
2015-01-19 17:02:30 +0100

    Begin BIM+ Download component

---
+++ IFCVisualiser/IFCVisualiser/BimPlusDownload.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+using IFCVisualiser.Server.BIMplus;
+using IFCVisualiser.Properties;
+
+namespace IFCVisualiser
+{
+    public class BimPlusDownload : GH_Component
+    {
+        // ##########################################################################################################################
+        private const string SName = "BimPlusDownload";
+        private const string SAbbreviation = "BimPlusDownload";
+        private const string SDescription = "Takes a BIM+ URI (and an optional user, password) and returns a string of the ifc file";
+        private const string SCategory = "KsdIFC";
+        private const string SSubCategory = "BIM+ Tools";
+        // ##########################################################################################################################
+
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public BimPlusDownload()
+            : base(SName, SAbbreviation, SDescription, SCategory, SSubCategory)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            // Input is the XML-File as String
+            pManager.AddTextParameter("URI", "@", "Address of the BIM+ model", GH_ParamAccess.item);
+            pManager.AddTextParameter("Username", "Username", "Username to log in the server", GH_ParamAccess.item);
+            pManager.AddTextParameter("Password", "Password", "Password to log in the server", GH_ParamAccess.item);
+            pManager.AddTextParameter("Serializer", "Serializer", "Serializer to get the model", GH_ParamAccess.item);
+
+            pManager[1].Optional = true;
+            pManager[2].Optional = true;
+            pManager[3].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("IfcFile", "FileLocation", "Path of the downloaded IFC file", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            String uri = "";
+
+            // Default values
+            String password = BIMPlusServer.Password;
+            String username = BIMPlusServer.Username;
+            //String serializer = BimPlusApi.Ifc2XSersializer;
+
+            DA.GetData<String>(0, ref uri);
+            DA.GetData(1, ref username);
+            DA.GetData(2, ref password);
+            //DA.GetData(3, ref serializer);
+
+            BIMPlusServer bimPlusServer = new BIMPlusServer();
+
+            try
+            {
+                bimPlusServer.authorize();
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                Console.WriteLine(e.FileName);
+            }
+
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return Resources.BimPlusDownload;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{bcb66c97-2f43-44c2-a969-af9733d4a6d4}"); }
+        }
+    }
+}
---
+++ IFCVisualiser/IFCVisualiser/Server/BIMplus/BIMPlusServer.cs
@@ -0,0 +1,58 @@
+﻿using BimPlus.LightCaseClient;
+using BimPlus.Sdk.Data.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace IFCVisualiser.Server.BIMplus
+{
+    class BIMPlusServer
+    {
+        public static String HostUrl = "https://www.bimplus.net";
+        public static String Username = "jana.pejic.9@gmail.com";
+        public static String Password = "testpassword";
+
+        public void authorize()
+        {
+            // Create authentication request, providing some user information 
+            AuthenticationAuthorizeRequest request = new AuthenticationAuthorizeRequest();
+            request.UserId = Username;
+            request.Password = Password;
+            request.ClientId = Guid.NewGuid();
+            request.ApplicationId = new Guid("FE9BE983-DF2C-4414-AD3F-B9A1EF6AB29A");
+
+            String serviceUrl = HostUrl + "/v2/authorize";
+            AuthenticationAuthorizeResponse response = null;
+
+            var config = GenericProxies.CreateDefaultConfiguration();
+
+            // Post the authorize request
+            try
+            {
+                response = GenericProxies.RestPost<AuthenticationAuthorizeResponse, AuthenticationAuthorizeRequest>(serviceUrl, request);
+                if (response != null)
+                {
+                    config.AuthorizationAccessToken = new Guid(response.AccessToken); // Store the returned access token
+                    config.AuthorizationTokenType = response.TokenType;
+                }
+            }
+            catch (WebException ex)
+            {
+                // Authorization failed
+                Console.WriteLine(ex.Message);
+                Console.Write("Press Enter to exit");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(String.Format("Hello {0}, welcome to bim+", Username));
+        }
+
+        public void exportIFC()
+        {
+
+        }
+    }
+}
